5d74a0d1bea0e5cc1dcb3323704ee266886f5309
seb <seb@Seb-PC>
2015-10-14 14:43:59 +0200

    GameGrid extends Parent

--- src/ctrl/GameGridControler.java
+++ src/ctrl/GameGridControler.java
@@ -13,35 +13,10 @@ import model.GameGrid;
  *
  * @author seb
  */
-public class GameGridControler implements GraphicControler{
+public class GameGridControler {
     private GameGrid gameGrid;
     
     public GameGridControler(GameGrid gameGrid) {
         this.gameGrid = gameGrid;
-    }
-
-    @Override
-    public void paintOn(GraphicsContext gc) {
-        gameGrid.paintOn(gc);
-    }
-
-    @Override
-    public Double getX() {
-        return gameGrid.getX();
-    }
-
-    @Override
-    public Double getY() {
-        return gameGrid.getY();
-    }
-
-    @Override
-    public void moveTo(Double x, Double y, Scene scene) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public boolean isOn(Double x, Double y) {
-        return gameGrid.isOn(x, y);
     }
 }
--- src/model/GameGrid.java
+++ src/model/GameGrid.java
@@ -1,106 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package model;
 
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.SimpleDoubleProperty;
-import javafx.scene.Scene;
-import javafx.scene.canvas.GraphicsContext;
+import java.util.ArrayList;
+import javafx.scene.Parent;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 /**
  *
- * @author localwsp
+ * @author seb
  */
-public class GameGrid implements Graphic {
-    private DoubleProperty x = new SimpleDoubleProperty(); // Le coin supérieur gauche du carré englobant
-    private DoubleProperty y = new SimpleDoubleProperty();
-    private DoubleProperty xPercentage = new SimpleDoubleProperty();
-    private DoubleProperty yPercentage = new SimpleDoubleProperty();
-    private DoubleProperty width = new SimpleDoubleProperty();
-    private DoubleProperty height = new SimpleDoubleProperty();
+public class GameGrid extends Parent {
+    public static int NBCOLUMNS = 7;
+    public static final int HEIGHT_BOTTOM = 50;
     
-    public GameGrid(int x, int y, int width, int height) {
-        this.x.set(x);
-        this.y.set(y);
-        this.width.set(width);
-        this.height.set(height);
+    private double posX;
+    private double posY;
+    private double width;
+    private double height;
+    private ArrayList<Rectangle> columns = new ArrayList<>();
+    private Rectangle bottom;
+    
+    public GameGrid(double posX, double posY, double width, double height) {
+        this.posX = posX;
+        this.posY = posY;
+        this.width = width;
+        this.height = height;
+        addColumns();
+        for(Rectangle r : columns) {
+            this.getChildren().add(r);
+        }
+        addBottom();
+        colorGrid(Color.WHITE, Color.WHITE);
     }
     
     public GameGrid() {
-        this(0,0,200,100);
+        this(100,100,500,600);
     }
     
-    @Override
-    public Double getX() {
-        return x.get();
-    }
-
-    @Override
-    public Double getY() {
-        return y.get();
-    }
-
-    @Override
-    public Double getWidth() {
-        return width.get();
-    }
-
-    @Override
-    public Double getHeight() {
-        return height.get();
-    }
-
-    @Override
-    public void scale(Double factor) {
-        width.set(width.get() * factor);
-        height.set(height.get() * factor);
-    }
-
-    @Override
-    public void move(Double directionX, Double directionY, Scene scene) {
-        x.set(x.get() + directionX);
-        y.set(y.get() + directionY);
-        recalculatePercentages(scene);
+    private void addColumns() {
+        columns = new ArrayList<>();
+        double startX = posX;
+        for(int i = 0; i < NBCOLUMNS; ++i) {
+            Rectangle rect = new Rectangle(
+                startX,
+                posY,
+                width/NBCOLUMNS,
+                height
+            );
+            startX += width/NBCOLUMNS;
+            columns.add(rect);
+        }
     }
     
-    private void recalculatePercentages(Scene scene) {
-        
-        this.xPercentage.set(x.get()/scene.getWidth());
-        x.set(scene.getWidth()*xPercentage.get());
-        
-        this.yPercentage.set(y.get()/scene.getHeight());
-        y.set(scene.getHeight()*yPercentage.get());
+    private void addBottom() {
+        bottom = new Rectangle(
+            posX, height + posY, width, HEIGHT_BOTTOM
+        );
+        this.getChildren().add(bottom);
     }
     
-    @Override
-    public void moveTo(Double x, Double y, Scene scene) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
-    @Override
-    public boolean isOn(Double x, Double y) {
-        Double rectX = this.x.get();
-        Double rectY = this.y.get();
-        Double rectWidth = width.get();
-        Double rectHeight = height.get();
-        return isOnRectangle(x, y, rectX, rectY, rectWidth, rectHeight);
-    }
-    
-    private boolean isOnRectangle(Double x, Double y, Double rectX, Double rectY,
-            Double rectWidth, Double rectHeight) {
-        return x >= rectX && x <= rectWidth &&
-               // Rappel: y devient négatif s'il augmente
-               y <= rectY && y >= rectHeight;
-    }
-
-    @Override
-    public void paintOn(GraphicsContext gc) {
-        gc.setFill(Color.BLUE);
-        gc.fillRect(gc.getCanvas().getWidth()*xPercentage.get(), 
-                    gc.getCanvas().getHeight()*yPercentage.get(), 
-                    width.get(), 
-                    height.get());
-        x.set(gc.getCanvas().getWidth()*xPercentage.get());
-        y.set(gc.getCanvas().getHeight()*yPercentage.get());
+    private void colorGrid(Color colorColumns, Color colorBottom) {
+        for(Rectangle r : columns) {
+            r.setFill(colorColumns);
+        }
+        bottom.setFill(colorBottom);
     }
     
 }
